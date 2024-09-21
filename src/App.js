cdce80a983b8935fd0246dbe2e5eb8ca32a9fdc9
NightDev <158066187+NightDev94@users.noreply.github.com>
2024-09-21 05:02:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import stitchImage from './images/pxfuel.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="app-container">
+      <div className="text-container">
+        <h1>Un poema para ti</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          En un rincón del cielo estrellado,<br />
+          donde el amor se vuelve infinito,<br />
+          soy tu Stitch, siempre a tu lado,<br />
+          con tu luz, mi mundo es más bonito.<br />
+          <br />
+          Eres mi Ángela, mi musa y mi anhelo,<br />
+          con tu risa y tu magia, me haces soñar,<br />
+          en tus brazos encuentro el más dulce cielo,<br />
+          juntos navegamos, listos para volar.<br />
+          <br />
+          Como yo, un poco loco y sincero,<br />
+          te sigo a cada paso, siempre leal,<br />
+          en cada travesura, un amor verdadero,<br />
+          nuestro lazo es fuerte, un hilo vital.<br />
+          <br />
+          Tus ojos son estrellas, destellos de vida,<br />
+          en cada mirada, un hogar hallé,<br />
+          y aunque a veces choquemos en esta vida,<br />
+          nuestro amor es un cuento que nunca se perderá.<br />
+          <br />
+          Así, en nuestra historia, tú eres mi encanto,<br />
+          la Ángela que abraza su ser especial,<br />
+          hoy celebro contigo, mi amor tanto,<br />
+          en este viaje, eres mi final ideal.<br />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
